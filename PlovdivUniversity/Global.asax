946726ac4be232aed73e630731f36376ca8f3143
Svetoslav Slavov <svetoslav_slavov@icloud.com>
2016-12-09 03:06:00 +0200

    First commits and changes for this project.

---
+++ PlovdivUniversity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PlovdivUniversity.MvcApplication" Language="C#" %>
---
+++ PlovdivUniversity/Models/ViewModel/InstructorIndexData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlovdivUniversity.Models.ViewModel
+{
+    public class InstructorIndexData
+    {
+        public IEnumerable<Models.Instructors> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
